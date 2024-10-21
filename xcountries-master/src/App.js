15e0a0b81aa0c71f96fb853b13dfbaef60015dda
aniket <shindeab2001@gmail.com>
2024-10-21 11:34:03 +0530

    Initial commit

---
+++ xcountries-master/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CountyFlags from './components/CountryFlags';
+
+function App() {
+  return (
+    <div className="App">
+      <CountyFlags />
+    </div>
+  );
+}
+
+export default App;
